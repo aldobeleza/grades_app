0b8a6a07d3ccec33fb2def838bcbce3266577c7a
aldo.beleza <aldobeluco@gmail.com>
2020-09-07 00:08:03 -0300

    qq

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://backenddesafio.herokuapp.com',
+  baseURL: 'https://backenddesafio.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
